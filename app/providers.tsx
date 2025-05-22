84a652c80888956d6d5bd273cdc18628eaf2a33c
Taylor Alex Bonneville <taylorbonneville@gmail.com>
2025-05-22 02:28:10 -0600

    test

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,13 +2,18 @@
 
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <TooltipProvider>
+        <Toaster />
+        {children}
+      </TooltipProvider>
     </QueryClientProvider>
   );
 } 
